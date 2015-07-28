3a0962c17af0043680155ab07bb5a11dff68d374
Rob McQueen <robmcqn@gmail.com>
2015-07-27 20:21:30 -0700

    Version bump 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='tessera-up',
-    version='0.0.1',
+    version='0.0.2',
     include_package_data=True,
     packages=['tessera_up'],
     author="Rob McQueen",
